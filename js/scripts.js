b33aa66996858357d4cb01c0e30e689a40ff4862
Andergomesz <andersongomesrc@gmail.com>
2021-05-19 22:15:34 -0300

    grafico 1

--- js/scripts.js
+++ js/scripts.js
@@ -68,32 +68,71 @@ var myChart = new Chart(ctx, {
 });
 
 
+
+
+
+
+
 var ctx = document.getElementById('grafico03').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+        labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio'],
+        datasets: [
+            {
+                label: 'Meta',
+                type: 'line',
+                data: [100, 100, 100, 100, 100,],
+                backgroundColor: [
+                    'rgba(3, 59, 38)'
+                ],
+                borderColor: [
+                    'rgba(3, 59, 38)'
+                ],
+                borderWidth: 2
+            },
+            
+            {
+            
+            label: '2019',
+            data: [67, 84, 94, 37, 67, 84],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(#d76b00)'
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(255, 99, 132, 1)'
             ],
             borderWidth: 1
-        }]
+        },
+
+        {
+            label: '2020',
+            data: [94, 97, 81, 79, 90, 98],
+            backgroundColor: [
+                'rgba(#0f4c81)'
+            ],
+            borderColor: [
+                'rgba(54, 162, 235, 1)'
+            ],
+            borderWidth: 1
+        },
+        {
+            label: '2021',
+            data: [100, 101, 95, 91, 107, 99],
+            backgroundColor: [
+                'rgba(#F5DF4D)'
+            ],
+            borderColor: [
+                'rgba(255, 206, 86, 1)'
+            ],
+            borderWidth: 1
+        }
+
+        
+    
+    
+    ]
+        
     },
     options: {
         scales: {
