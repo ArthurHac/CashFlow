c180fae865ed3a79e56de4d6d9c335bffefd1b41
ArthurHac <80919411+ArthurHac@users.noreply.github.com>
2021-05-24 22:28:47 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,203 @@
+var ctx = document.getElementById('grafico01');
+var myChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ['Entrada','Saida'],
+        datasets: [{
+            label: 'Entrada x Saida',
+            data: [50, 10,],
+            backgroundColor: [
+                'rgba(50,205,50)',
+                'rgba(220,20,60)'
+            ],
+            borderColor: [
+                'rgba(50,205,50)',
+                'rgba(220,20,60)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        plugins: {
+        title: {
+            display: true,
+            text: "Entrada x Saida"
+        },
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+}
+});
 
-var salvarProduto = function(){
+
+var ctx = document.getElementById('grafico02');
+var myChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ['Em Estoque', 'Em Falta'],
+        datasets: [{
+            label: 'Estoque Atual',
+            data: [245, 100],
+            backgroundColor: [
+                'rgba(0,191,255)',
+                'rgba(255,99,71)',
+            ],
+            borderColor: [
+                'rgba(0,191,255)',
+                'rgba(255,99,71)',
+            ],
+            borderWidth: 2
+        },]
+    },
+    options: {
+        plugins: {
+            title: {
+                display: true,
+                text: "Estoque Atual"
+            },
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    }
+});
+
+
+
+
+
+
+
+var ctx = document.getElementById('grafico03').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio'],
+        datasets: [
+            {
+                label: 'Meta',
+                type: 'line',
+                data: [100, 100, 100, 100, 100,],
+                backgroundColor: [
+                    'rgba(3, 59, 38)'
+                ],
+                borderColor: [
+                    'rgba(3, 59, 38)'
+                ],
+                borderWidth: 2
+            },
+            
+            {
+            
+            label: '2019',
+            data: [67, 84, 94, 37, 67, 84],
+            backgroundColor: [
+                'rgba(#d76b00)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)'
+            ],
+            borderWidth: 1
+        },
+
+        {
+            label: '2020',
+            data: [94, 97, 81, 79, 90, 98],
+            backgroundColor: [
+                'rgba(#0f4c81)'
+            ],
+            borderColor: [
+                'rgba(54, 162, 235, 1)'
+            ],
+            borderWidth: 1
+        },
+        {
+            label: '2021',
+            data: [100, 101, 95, 91, 107, 99],
+            backgroundColor: [
+                'rgba(#F5DF4D)'
+            ],
+            borderColor: [
+                'rgba(255, 206, 86, 1)'
+            ],
+            borderWidth: 1
+        }
+
+        
+    
+    
+    ]
+        
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+
+
+
+
+sessionStorage.key(1)
+
+const salvarProduto = document.getElementById('button_salvar_produto').onclick = () => {
+    console.log("show")
     var cadastroProdutos = document.getElementById ('cadastroProdutos').value;
     var cadastroEAN = document.getElementById ('cadastroEAN').value;
     var cadastroCategoria = document.getElementById ('cadastroCategoria').value;
     var cadastroQuantidade = document.getElementById ('cadastroQuantidade').value;
     var cadastroFornecerdor = document.getElementById ('cadastroFornecerdor').value;
-    sessionStorage.key(1)
-    const getId = JSON.parse(localStorage.getItem(sessionStorage.key(1),id).value)
-    const produto = { 
+    
+    const dadoProduto = JSON.parse(localStorage.getItem(sessionStorage.getItem(0)))
+    const getId = dadoProduto.produto.length
+    const componente = { 
         nome: cadastroProdutos,
         codEAN: cadastroEAN,
-        categoria: {},
+        categoria: cadastroCategoria,
         quantidade: cadastroQuantidade,
         fornecedor: cadastroFornecerdor
     }
 
-    const dadoProduto = JSON.parse(localStorage.getItem(sessionStorage.key(1)))
-    dadoProduto.produto = produto
 
-
-    localStorage.setItem(sessionStorage.key(1),JSON.stringify(produto))
-    
+    dadoProduto.produto[0] = getId
+    dadoProduto.produto.push(componente)
+    localStorage.setItem(sessionStorage.getItem(0),JSON.stringify(dadoProduto))
 }
 
-document.getElementById('button_salvar_produto').click = salvarProduto;
+
+const adicionarHTMLproduto  = () => {
+    
+    const prod = JSON.parse(localStorage.getItem(sessionStorage.getItem(0)))
+    const prodPropriedade = prod.produto
+    
 
 
+    for(i = 1; i < prod.produto.length; i++){
+        
+        const node = document.createElement("li")
+        node.className = "list-group-item mt-4" 
+        node.innerHTML = `
+        <p>${prodPropriedade[i].nome}</p>
+        <p>${prodPropriedade[i].categoria}</p>
+        <p>${prodPropriedade[i].fornecedor}</p>
+        <p>${prodPropriedade[i].quantidade}</p>
+        ` 
+        document.getElementById("produto_html").appendChild(node)
+    }
+}
+
+
+document.getElementById('button_salvar_produto').onclick = salvarProduto;
+document.getElementById('button_down').onclick = adicionarHTMLproduto
 
 var ctx = document.getElementById('graficoEstoque').getContext('2d');
 var myChart = new Chart(ctx, {
