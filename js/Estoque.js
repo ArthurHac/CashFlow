e4156bee6e1eeed565867a8a8bcd6808ae4095a0
ArthurHac <80919411+ArthurHac@users.noreply.github.com>
2021-06-17 23:32:52 -0300

    dados

--- js/Estoque.js
+++ js/Estoque.js
@@ -21,7 +21,7 @@ const salvarProduto = document.getElementById('button_salvar_produto').onclick =
         categoria: cadastroQuantidade,
         quantidade: cadastroCategoria,
         fornecedor: cadastroFornecedor,
-        preco: cadastroValor.replace(',','.')
+        preco: cadastroValor.replace(',', '.')
     }
 
     dadoProduto.produto[0] = getId
@@ -43,68 +43,76 @@ var ctx = 'graficoDoEstoque';
 var ctx = document.getElementById('graficoDoEstoque');
 
 function graficoEstoque() {
-        if(typeof myChart != 'undefined'){
-            myChart.destroy()
-        }
+    if (typeof myChart != 'undefined') {
+        myChart.destroy()
+    }
 
-        myChart = new Chart(ctx, {
-            type: 'doughnut',
-            data:
-            {
-                labels: [],
-                datasets: [{
-                    label: '',
-                    data: [],
-                    backgroundColor: [
-                        '#ff595e',
-                        '#ffca3a',
-                        '#8ac926',
-                        '#1982c4',
-                        '#1982c4',
-                        '#6a4c93',
-                        '#f72585',
-                        '#b5179e',
-                        '#7209b7',
-                        '#560bad',
-                        '#480ca8',
-                        '#3a0ca3',
-                        '#3f37c9',
-                        '#4361ee',
-                        '#4895ef',
-                        '#b7094c',
-                        '#723c70',
-                        '#455e89',
-                        '#1780a1',
-                        '#1d4e89',
-                        '#3fc1c0',
-                        '#6a00f4',
-                        '#54478c',
-                        '#048ba8',
-                        '#b9e769'
-                    ],
+    myChart = new Chart(ctx, {
+        type: 'doughnut',
+        data:
+        {
+            labels: [],
+            datasets: [{
+                label: '',
+                data: [],
+                backgroundColor: [
+                    '#ff595e',
+                    '#ffca3a',
+                    '#8ac926',
+                    '#1982c4',
+                    '#1982c4',
+                    '#6a4c93',
+                    '#f72585',
+                    '#b5179e',
+                    '#7209b7',
+                    '#560bad',
+                    '#480ca8',
+                    '#3a0ca3',
+                    '#3f37c9',
+                    '#4361ee',
+                    '#4895ef',
+                    '#b7094c',
+                    '#723c70',
+                    '#455e89',
+                    '#1780a1',
+                    '#1d4e89',
+                    '#3fc1c0',
+                    '#6a00f4',
+                    '#54478c',
+                    '#048ba8',
+                    '#b9e769'
+                ],
 
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                responsive: true,
-                plugins: {
-                    title: {
-                        display: true,
-                        text: 'Quantidade de produtos por categorias'
-                    }
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Quantidade de produtos por categorias'
                 }
             }
-        });
-        
-        let dados = JSON.parse(localStorage.getItem(sessionStorage.getItem(0)))
-        for(i=1; i < dados.produto.length; i++){
-            myChart.data.labels.push(dados.produto[i].nome)
-            myChart.data.datasets[0].data[i-1] = dados.produto[i].quantidade
         }
+    });
 
-        myChart.update()
+    let dados = JSON.parse(localStorage.getItem(sessionStorage.getItem(0)))
+    for (i = 1; i < dados.produto.length; i++) {
+        myChart.data.labels.push(dados.produto[i].nome)
+        myChart.data.datasets[0].data[i - 1] = dados.produto[i].quantidade
     }
+
+    myChart.update()
+
+    document.getElementById('cadastroProduto').value = ""
+    document.getElementById('cadastroEAN').value = ""
+    document.getElementById('cadastroCategoria').value = ""
+    document.getElementById('cadastroQuantidade').value = ""
+    document.getElementById('cadastroFornecedor').value = ""
+    document.getElementById('cadastroValor').value= ""
+
+}
 
 
 
@@ -114,18 +122,18 @@ function graficoEstoque() {
 // funcao master
 
 function dadosProdutos() {
-    
+
     let selectHtml = document.getElementById('produtos')
     let dados = JSON.parse(localStorage.getItem(sessionStorage.getItem(0)))
     let txt = ""
     let quantEstoque = 0
     let quantProduto = 0
-    let valorTotal = 0 
+    let valorTotal = 0
 
     for (i = 1; i < dados.produto.length; i++) {
         quantProduto++
         quantEstoque = quantEstoque + Number.parseInt(dados.produto[i].quantidade)
-        valorTotal = valorTotal + Number.parseFloat( dados.produto[i].preco) * quantEstoque
+        valorTotal = valorTotal + Number.parseFloat(dados.produto[i].preco) * quantEstoque
         txt = txt + `
         <div class="accordion" id="accordionPanelsStayOpenExample">
   <div class="accordion-item">
@@ -164,57 +172,56 @@ function dadosProdutos() {
     }
     document.getElementById('quantProduto').innerHTML = quantProduto
     document.getElementById('quantEstoque').innerHTML = quantEstoque
-    document.getElementById('valorEstoque').innerHTML = "R$ "+valorTotal.toFixed(2)
-    document.getElementById('ProdutoRetirado').innerHTML = "R$ "+ dados.ValorVendido
+    document.getElementById('valorEstoque').innerHTML = "R$ " + valorTotal.toFixed(2)
+    document.getElementById('ProdutoRetirado').innerHTML = "R$ " + dados.ValorVendido
     selectHtml.innerHTML = txt
 }
 
 
-function valorEstoque(){
+function valorEstoque() {
     let vendaProduto = document.getElementById('informaVendaProduto').value
     let vendaQuantidade = document.getElementById('informaQuantidade').value
-    let dados = JSON.parse(localStorage.getItem(sessionStorage.getItem(0))) 
+    let dados = JSON.parse(localStorage.getItem(sessionStorage.getItem(0)))
 
-    for(i = 1; i < dados.produto.length; i++){
-        if(dados.produto[i].nome == vendaProduto){
-            if(Number.parseInt(dados.produto[i].quantidade) >= vendaQuantidade){
-            dados.produto[i].quantidade = Number.parseInt(dados.produto[i].quantidade) - vendaQuantidade
-            dados.ValorVendido = dados.ValorVendido + Number.parseFloat(dados.produto[i].preco.replace(',','.')) * vendaQuantidade
-            }else{
+    for (i = 1; i < dados.produto.length; i++) {
+        if (dados.produto[i].nome == vendaProduto) {
+            if (Number.parseInt(dados.produto[i].quantidade) >= vendaQuantidade) {
+                dados.produto[i].quantidade = Number.parseInt(dados.produto[i].quantidade) - vendaQuantidade
+                dados.ValorVendido = dados.ValorVendido + Number.parseFloat(dados.produto[i].preco.replace(',', '.')) * vendaQuantidade
+            } else {
                 alert('quantidade insuficiente')
             }
         }
     }
-    document.getElementById('ProdutoRetirado').innerHTML = "R$ "+ dados.ValorVendido
+    document.getElementById('ProdutoRetirado').innerHTML = "R$ " + dados.ValorVendido
     localStorage.setItem(sessionStorage.getItem(0), JSON.stringify(dados))
 }
 
 function excluirProduto(dadosExcluir) {
     console.log(dadosExcluir.getAttribute('id'))
     let dados = JSON.parse(localStorage.getItem(sessionStorage.getItem(0)))
-    for(i=1; i < dados.produto.length;i++){
-        if(dados.produto[i].nome == dadosExcluir.getAttribute('id')){
+    for (i = 1; i < dados.produto.length; i++) {
+        if (dados.produto[i].nome == dadosExcluir.getAttribute('id')) {
             dados.produto.splice(i, 1)
-            localStorage.setItem(sessionStorage.getItem(0),JSON.stringify(dados))
+            localStorage.setItem(sessionStorage.getItem(0), JSON.stringify(dados))
         }
     }
-
     myChart.update()
     dadosProdutos()
     graficoEstoque()
 
 }
 
-function itemModal(){
-    let dados = JSON.parse(localStorage.getItem(sessionStorage.getItem(0))) 
+function itemModal() {
+    let dados = JSON.parse(localStorage.getItem(sessionStorage.getItem(0)))
     let html = document.getElementById('informaVendaProduto')
     let txt = ``
 
-    for(i=1;i < dados.produto.length; i++){
+    for (i = 1; i < dados.produto.length; i++) {
         console.log(1)
         txt = txt + `
             <option value="${dados.produto[i].nome}">${dados.produto[i].nome}</option>
-        ` 
+        `
     }
 
     html.innerHTML = txt
@@ -223,7 +230,7 @@ function itemModal(){
 
 
 
-window.onload = () => { 
+window.onload = () => {
     dadosProdutos()
     graficoEstoque()
     itemModal()
@@ -245,5 +252,8 @@ document.getElementById('Adicionar_venda_produto').onclick = () => {
     myChart.update()
     dadosProdutos()
     graficoEstoque()
+
+
+    
 }
 
