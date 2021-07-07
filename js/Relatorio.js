f65be493d51cd8f35f8219e2a94cd7b05f4ee90c
Andergomesz <andersongomesrc@gmail.com>
2021-07-07 00:18:39 -0300

    Relatorio

--- js/Relatorio.js
+++ js/Relatorio.js
@@ -24,37 +24,51 @@ function infoPerfil(){
     `
 }
 //
-function demoFromHTML() {
-    let pdf = new jsPDF('p', 'pt', 'letter');
 
-    source = $('#content')[0];
-    specialElementHandlers = {
-        '#bypassme': function (element, renderer) {
-            return true
-        }
-    };
-    margins = {
-        top: 80,
-        bottom: 60,
-        left: 40,
-        width: 522
-    };
+jQuery(function ($) {
+    $("#exportButton").click(function () {
+        // parse the HTML table element having an id=exportTable
+        var dataSource = shield.DataSource.create({
+            data: "#exportTable",
+            schema: {
+                type: "table",
+                fields: {
+                    Name: { type: String },
+                    Age: { type: Number },
+                    Email: { type: String }
+                }
+            }
+        });
 
-    pdf.fromHTML(
-        source,
-        margins.left,
-        margins.top, {
-        'width': margins.width,
-        'elementHandlers': specialElementHandlers
-    },
+        // when parsing is done, export the data to PDF
+        dataSource.read().then(function (data) {
+            var pdf = new shield.exp.PDFDocument({
+                author: "PrepBootstrap",
+                created: new Date()
+            });
 
-        function (dispose) {
-            pdf.save('relatorio.pdf');
-        }, margins
-    );
-}
+            pdf.addPage("a4", "portrait");
 
-window.onload = () => {
-    perfil()
-    infoPerfil()
-}
+            pdf.table(
+                50,
+                50,
+                data,
+                [
+                    { field: "Name", title: "Person Name", width: 200 },
+                    { field: "Age", title: "Age", width: 50 },
+                    { field: "Email", title: "Email Address", width: 200 }
+                ],
+                {
+                    margins: {
+                        top: 50,
+                        left: 50
+                    }
+                }
+            );
+
+            pdf.saveAs({
+                fileName: "PrepBootstrapPDF"
+            });
+        });
+    });
+});
