51c67fb99d9a89f28f033bcfcf4598747a59908e
Andergomesz <andersongomesrc@gmail.com>
2021-07-07 23:51:39 -0300

    a

--- js/Relatorio.js
+++ js/Relatorio.js
@@ -1,3 +1,5 @@
+sessionStorage.key(1)
+
 function perfil(){
     let dados = JSON.parse(localStorage.getItem(sessionStorage.getItem(0)))
     document.getElementById('ftoPerfilid').style.backgroundImage = `url('${dados.infoUsuario.ftoPerfil}')`
@@ -27,20 +29,18 @@ function infoPerfil(){
 
 jQuery(function ($) {
     $("#exportButton").click(function () {
-        // parse the HTML table element having an id=exportTable
         var dataSource = shield.DataSource.create({
             data: "#exportTable",
             schema: {
                 type: "table",
                 fields: {
                     Name: { type: String },
-                    Age: { type: Number },
+                    Age: { type: String },
                     Email: { type: String }
                 }
             }
         });
 
-        // when parsing is done, export the data to PDF
         dataSource.read().then(function (data) {
             var pdf = new shield.exp.PDFDocument({
                 author: "PrepBootstrap",
@@ -67,8 +67,60 @@ jQuery(function ($) {
             );
 
             pdf.saveAs({
-                fileName: "PrepBootstrapPDF"
+                fileName: "Relatório"
             });
         });
     });
 });
+
+
+
+function dadosRelatorio() {
+
+    let selectHtml = document.getElementById('relatorioFornecedor')
+    let dados = JSON.parse(localStorage.getItem(sessionStorage.getItem(0)))
+    let txt = ""
+    for (i = 1; i <dados.relatorios.length; i++) {
+      txt = txt +
+  
+        `
+  
+        <tr>
+        <td id="nomeFantasiaRelatorio">${dados.relatorios[i].nomeFantasiaRelat}</td>
+    </tr>
+    <tr>
+        <td id="cnpjFornecedorRelat">${dados.relatorios[i].cnpjFornecedor}</td>
+    </tr>
+    <tr>
+        <td id="responsavelRelatorio">${dados.relatorios[i].responsavelRelat}</td>
+    </tr>
+    <tr>
+        <td id="contatoRelatorio">${dados.relatorios[i].contatoRelat}</td>
+    </tr>
+    <tr>
+        <td id="mailRelatorio">${dados.relatorios[i].mailRelat}</td>
+    </tr>
+
+      
+      `
+    }
+  
+    selectHtml.innerHTML = txt
+  }
+  
+
+
+
+  window.onload = () => {
+    dadosRelatorio()
+}
+
+const dadosFornecedor = JSON.parse(localStorage.getItem(sessionStorage.getItem(0)))
+const getId = dadosFornecedor.fornecedor.length
+const componente = {
+    nomeFantasiaRelat: nomeFantasiaRelatorio,
+    cnpjFornecedorRelat: cnpjFornecedorRelat,
+    responsavelRelat: responsavelRelatorio,
+    contatoRelat: contatoRelatorio,
+    mailRelat: mailRelatorio,
+}
